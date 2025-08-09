82c6508b144b57ef13aa19d2b47ffb0c15a6b51e
Ganesh S <ganesh262k03@gmail.com>
2025-08-09 12:42:35 +0530

    4th commit

---
+++ cart.js
@@ -0,0 +1,21 @@
+function keyDown(event) {
+    if (event.key === "Enter") {
+        calculation();
+    }
+}
+
+function calculation() {
+    const inputElement = document.querySelector('.input-element');
+    let cost = Number(inputElement.value); 
+if(cost != Number(inputElement.value)){
+ document.querySelector('.total').innerHTML =`please enter valid cost`
+ return
+}
+  if (cost > 0 && cost <= 40) {
+        cost += 10; 
+    } else if (cost > 40 && cost <= 60) {
+        cost += 5; 
+    }
+
+    document.querySelector('.total').innerHTML = `Total Cost : $${cost}`;
+}
